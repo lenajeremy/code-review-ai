b138c55eeeeba4e287618585b6549f2903e003a2
Jeremiah Lena <jeremiahlena13@gmail.com>
2024-08-07 23:47:45 +0100

    genai

--- main.go
+++ main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/google/generative-ai-go/genai"
+	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func init() {
+	if godotenv.Load() != nil {
+		log.Fatal("Failed to setup .env")
+	}
+}
+
+var ctx = context.Background()
 
 func main() {
-	fmt.Println("Welcome to this guy")
+	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("GEMINI_API_KEY")))
+
+	defer func(client *genai.Client) {
+		err := client.Close()
+		if err != nil {
+			log.Fatal("Failed to close client")
+		}
+	}(client)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	model := client.GenerativeModel("gemini-1.5-flash")
+
+	//settings := genai.SafetySetting{Category: genai.HarmCategoryUnspecified, Threshold: genai.HarmBlockNone}
+	//model.SafetySettings = []*genai.SafetySetting{&settings}
+
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/ai", func(c *gin.Context) {
+		resp, err := model.GenerateContent(ctx, genai.Text("Write a letter to my mother telling her i love her"))
+
+		if err != nil {
+			log.Println("Error with model")
+			log.Fatal(err)
+			return
+		}
+
+		if story, err := json.Marshal(resp.Candidates[0].Content.Parts[0]); err != nil {
+			c.String(http.StatusInternalServerError, err.Error())
+		} else {
+			log.Println(strings.Split(string(story), "\n"))
+			c.HTML(http.StatusOK, "index.tmpl", gin.H{
+				"story": strings.Join(strings.Split(string(story), "\n"), "<br />"),
+			})
+		}
+
+		return
+	})
+
+	log.Fatal(r.Run(":3000"))
 }
